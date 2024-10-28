4c5829487271f9788ba7381713392eff70d98ea6
Lê Anh khoa <22162016@student.hcmute.edu.vn>
2024-10-28 16:02:58 +0700

    first commit

--- src/main/java/hisc/com/spring_2810/controller/HomeController.java
+++ src/main/java/hisc/com/spring_2810/controller/HomeController.java
@@ -1,14 +1,16 @@
 package hisc.com.spring_2810.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home(){
-        return "Hello World!";
+        return "layout/main_layout";
     }
 }
 
